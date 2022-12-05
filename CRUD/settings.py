ec4c8264ca41fa931bdc3686ea66c64018b257d2
Bishal Mahanta <thegrtbsl@gmail.com>
2022-12-05 14:25:45 +0530

    added static files

--- CRUD/settings.py
+++ CRUD/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -123,9 +124,16 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATIC_ROOT = BASE_DIR / "static"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_URL = "login"
+
+# for railway deployment
+
+CSRF_TRUSTED_ORIGINS = ["https://djangotodoapp-production.up.railway.app"]
